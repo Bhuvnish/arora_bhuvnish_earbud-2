7f561241127eda6d3fa1766ad2fa9e232c835a90
Bhuvnish <b_arora163217@fanshaweonline.ca>
2023-11-10 05:24:14 -0500

    making scrolling animation work

--- js/main.js
+++ js/main.js
@@ -48,7 +48,51 @@
     //document.body.addEventListener('touchmove', onMove);
     
     })();
-    
+    const canvas = document.querySelector("#explode-view");
+    const context = canvas.getContext("2d");
+    canvas.width = 1920;
+    canvas.height = 1080;
+    const frameCount = 1080; //how many still frames do we have?
+    const images = []; //an array to hold all of our images
+    //create an object literal with a property frame to hold the current frame
+    const buds = {
+        frame: 0
+    };
+
+    for (let i=0; i<frameCount; i++) {
+        //console.log(i);
+        //const img = new Image();
+        const img = document.createElement("img");
+        //need to recreate a string: images/explode_0001.webp
+        img.src = `images/imagesssss_${(i+1).toString().padStart(5, '0')}.jpg`;
+        images.push(img);
+    }
+    //console.table(images)
+
+    //Not actually aniamting a DOM element, but rather an object
+    //which contains a frame count
+    gsap.to(buds, {
+        frame: 1080,
+        snap: "frame",
+        scrollTrigger: {
+            trigger: "#explode-view",
+            pin: true,
+            scrub: 1,
+            start: "top top",
+            markers: true
+        },
+        onUpdate: render
+    })
+
+    images[0].addEventListener("onload", render);
+
+    function render() {
+        // console.log(buds.frame);
+        //console.log(images[buds.frame]);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(images[buds.frame], 0, 0);
+    }
+
     
     /*231-187.5 = 43.5.  43.5 is how much of the car is left showing*/
     
